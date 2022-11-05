20dcd27d875f4575167bad21314a32dafd27c5a3
Josna M Alexander <84304028+Josnaalex@users.noreply.github.com>
2022-11-05 23:18:51 +0530

    Update stackarr.c

--- stackarr.c
+++ stackarr.c
@@ -1,19 +1,57 @@
 #include<stdio.h>
 #include<stdlib.h>
 int a[50],top,maxsize,i,c,n;
-void push();
-void pop();
-void display();
+
+void push()
+{
+if(top==maxsize-1)
+printf("Stack overflow\n");
+else
+{
+  
+  top=top+1;
+  
+  printf("Enter stack element:");
+  scanf("%d",&n);
+  a[top]=n;
+  
+}
+}
+void display()
+{
+  printf("The stack elements are\n");
+  for(i=top;i>=0;i--)
+  {
+  printf("%d\n",a[i]);
+ 
+  }
+}
+void pop()
+{
+int del;
+if(top==-1)
+printf("Stack underflow\n");
+else
+{
+ del=a[top];
+ printf("popped element is %d\n",del);
+ 
+ top--;
+}
+}
+
 void main()
 {
 top=-1;
 maxsize=5;
 while(c!=4)
 {
-printf("1.push\n");
-printf("2.pop\n");
-printf("3.display stack\n");
-printf("enter choice");
+printf("------Menu-------\n");
+printf("1.Push\n");
+printf("2.Pop\n");
+printf("3.Display stack\n");
+printf("4.Exit\n");
+printf("enter choice:");
 scanf("%d",&c);
 switch(c)
 {
@@ -36,44 +74,6 @@ default:
 }
 
 }
-void push()
-{
-if(top==maxsize-1)
-printf("Stack overflow");
-else
-{
-  
-  top=top+1;
-  
-  printf("Enter stack element");
-  scanf("%d",&n);
-  a[top]=n;
-  
-}
-}
-void display()
-{
-  printf("The stack elements are\n");
-  for(i=top;i>=0;i--)
-  {
-  printf("%d\n",a[i]);
- 
-  }
-}
-void pop()
-{
-int del;
-if(top==-1)
-printf("Stack underflow");
-else
-{
- del=a[top];
- printf("popped element is %d\n",del);
- 
- top--;
-}
-}
-
 
 
   
