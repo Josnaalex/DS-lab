8d70ca616119e3de450076ad80769c64845c73ac
Josna M Alexander <84304028+Josnaalex@users.noreply.github.com>
2022-11-05 13:50:58 +0530

    Update merge.c

--- merge.c
+++ merge.c
@@ -2,17 +2,16 @@
 void main()
 {
 int a[20],b[20],c[50],m,n,i,j;
-printf("enter the size of 1st array");
+printf("enter array size:");
 scanf("%d",&n);
-printf("Enter the array elements");
+printf("Enter 1st array elements");
  for(i=0;i<n;i++)
  {
   scanf("%d",&a[i]);
  }
-printf("enter the size of 2nd array");
-scanf("%d",&m);
-printf("Enter the array elements");
- for(i=0;i<m;i++)
+
+printf("Enter 2nd array elements");
+ for(i=0;i<n;i++)
  {
   scanf("%d",&b[i]);
  }
@@ -21,7 +20,7 @@ for(i=0,j=0;i<n||j<n;i=i+2,j++)
 c[i]=a[j];
 c[i+1]=b[j];
 }
-for(i=0;i<(n+m);i++)
+for(i=0;i<(n+n);i++)
 {
  printf("%d",c[i]);
 }
