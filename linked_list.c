eca3f3b78368f6f38cf36345cf8a136893daf11c
Josna M Alexander <84304028+Josnaalex@users.noreply.github.com>
2022-11-06 22:43:05 +0530

    Update linked_list.c

--- linked_list.c
+++ linked_list.c
@@ -9,7 +9,7 @@ struct node *newnode,*head=NULL;
 void beg()
 {
 	newnode=(struct node *)malloc(sizeof(struct node));
-	printf("enter the data");
+	printf("enter the data:");
  	scanf("%d",&newnode->data);
 	newnode->next=head;
 	head=newnode;
@@ -18,7 +18,7 @@ void traversal()
 {
 	struct node *temp;
 	if(head==NULL)
-	printf("List is empty");
+	printf("List is empty\n");
 	else
 	{
 		temp=head;
@@ -32,7 +32,7 @@ void traversal()
 void end()
 {       struct node *temp;
  	newnode=(struct node *)malloc(sizeof(struct node));
- 	printf("enter the data");
+ 	printf("enter the data:");
 	scanf("%d",&newnode->data);
  	newnode->next=NULL;
  	temp=head;
@@ -49,10 +49,12 @@ void end()
 }
 int count()
 {
+    struct node *temp;
 	int count=0;
 	temp=head;
-	while(temp->next!=NULL)
+	while(temp!=NULL)
 	{
+	    temp=temp->next;
 		count++;
 	}
 	return count;
@@ -60,20 +62,33 @@ int count()
 void position()
 {
 	struct node *temp;
-	int pos;
+	int pos,i;
+	temp=head;
  	newnode=(struct node *)malloc(sizeof(struct node));
- 	printf("enter the data");
+ 	printf("enter the data:");
 	scanf("%d",&newnode->data);
-	printf("Enter the position where node is to be inserted");
+	printf("Enter the position where node is to be inserted:");
 	scanf("%d",&pos);
+	pos=pos-1;
 	if(pos>count())
+	printf("Invalid position");
+	else
+	{
+	    for(i=0;i<pos-1;i++)
+	    {
+	      temp=temp->next;  
+	    }
+	    newnode->next=temp->next;
+	    temp->next=newnode;
+	    
+	}
 	
 }
 void main()
 {
-	int c;
+	int c,x;
  
- 
+   
  	while(1)
 	{
  		printf("1.insertion at beginning\n");
@@ -91,17 +106,17 @@ void main()
  		case 2:
  			end();
 		 	break;
- 		/*case 3:
- 		position();
- 		break;*/
+ 		case 3:
+ 			position();
+ 			break;
 		case 4:
  			traversal();
-                        break;
+         		break;
+         
 		case 5:
  			exit(0);
  		default:printf("wrong choice");
 }
 }
 }
-
 
